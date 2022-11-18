a3873c7d91df6f6f095bdd605489d5c5e4d68a96
Shishir Islam shanto <shishir8089@gmail.com>
2022-11-18 10:26:21 +0600

    Add .envfile

--- sendemail/settings.py
+++ sendemail/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_jvw(t2c*a=%$k9w&ol(qh4_sjs=-an01=*y)n#$=*peqnbu%&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,11 +124,11 @@ STATICFILES_DIRS = [BASE_DIR, 'static']
 
 # email sending configaretion
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'demowebsite8080@gmail.com'
+EMAIL_HOST_USER = config('EMAIL_USER')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'eizlbhsuljqiryka'
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 
 
 # Default primary key field type
